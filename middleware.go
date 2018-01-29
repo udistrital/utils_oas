d65baad6f4e8947182db365f4a74fa188ca8b851
Javier Reyes <ingjsreyesm@gmail.com>
2018-01-29 14:37:02 -0500

    Se agrega archivo que contiene la función para enviar a RabbitMQ

--- middleware.go
+++ middleware.go
@@ -60,7 +60,7 @@ func FunctionAfterExec(ctx *context.Context) {
 	fmt.Println(data_response["json"])                                                                      //En las peticiones get y post se ve la data, devuelve OK cuando se hace un post o un delete
 	*/
 
-	var mensaje = fmt.Sprintf("{'FechaOperacion': '%s', 'User': 'userWSO2', 'IpUser': '%s', 'UrlService': '%s', 'Método': '%s', 'HostApi': '%s' ,'DataUser':'%s', 'DataResponse':'%s', 'ApiName':'%s'}", now, ip_user, url, metodo, host, data_user, data_response["json"], app[0])
+	var mensaje = fmt.Sprintf(`{"FechaOperacion": %s, "User": "userWSO2", "IpUser": %s, "UrlService": %s, "Método": %s, "HostApi": %s ,"DataUser":%s, "DataResponse":%s, "ApiName":%s}`, now, ip_user, url, metodo, host, data_user, data_response["json"], app[0])
 
 	sentToRabbit(mensaje)
 
@@ -68,60 +68,10 @@ func FunctionAfterExec(ctx *context.Context) {
 
 }
 
-func sentToRabbit(msj string) {
-
-	//p := beego.AppConfig.Strings("RABBIT_MQ_URI")
-
-	//Obtengo el parametro de configuración del API
-
-	con, err := amqp.Dial("amqp://guest:guest@10.20.0.175:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer con.Close()
-
-	connection = con
-
-	chanel, err := con.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer chanel.Close()
-
-	chl = chanel
-
-	cha := beego.AppConfig.Strings("RABBIT_MQ_CHANNEL")
-
-	fmt.Println(cha)
-
-	q, err := chl.QueueDeclare(
-		cha[0], // name
-		true,   // durable
-		false,  // delete when usused
-		false,  // exclusive
-		false,  // no-wait
-		nil,    // arguments
-	)
-	beego.Info(q)
-	failOnError(err, "Failed to declare a queue")
-
-	body := msj
-	err = chl.Publish(
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
-	failOnError(err, "Failed to publish a message")
-}
 func FunctionFinishRouter(ctx *context.Context) {
 	beego.Info("beego.FinishRouter: After finishing router")
 }
 
 func InitMiddleware() {
-
-	//beego.InsertFilter("*", beego.BeforeStatic, FunctionBeforeStatic, false)
-	//beego.InsertFilter("*", beego.BeforeRouter, FunctionBeforeRouter, false)
-	//beego.InsertFilter("*", beego.BeforeExec, FunctionBeforeExec, false)
 	beego.InsertFilter("*", beego.AfterExec, FunctionAfterExec, false)
-	//beego.InsertFilter("*", beego.FinishRouter, FunctionFinishRouter, false)
 }
