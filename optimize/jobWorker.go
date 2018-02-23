14c2147934bd956c139d58ea93d7b044f4a1be6a
miguelramirez93 <darckzios@gmail.com>
2018-02-23 13:06:22 -0500

    se agrega patron jobWorker

---
+++ optimize/jobWorker.go
@@ -0,0 +1,83 @@
+package optimize
+
+var WorkQueue = make(chan WorkRequest, 100)
+var WorkerQueue chan chan WorkRequest
+
+type WorkRequest struct {
+	JobParameter interface{}
+	Job          func(...interface{}) interface{}
+}
+
+func NewWorker(id int, workerQueue chan chan WorkRequest) Worker {
+	// Create, and return the worker.
+	worker := Worker{
+		ID:          id,
+		Work:        make(chan WorkRequest),
+		WorkerQueue: workerQueue,
+		QuitChan:    make(chan bool)}
+
+	return worker
+}
+
+type Worker struct {
+	ID          int
+	Work        chan WorkRequest
+	WorkerQueue chan chan WorkRequest
+	QuitChan    chan bool
+}
+
+// This function "starts" the worker by starting a goroutine, that is
+// an infinite "for-select" loop.
+func (w *Worker) Start() {
+	go func() {
+		for {
+			// Add ourselves into the worker queue.
+			w.WorkerQueue <- w.Work
+
+			select {
+			case work := <-w.Work:
+				work.Job(work.JobParameter)
+			case <-w.QuitChan:
+				// We have been asked to stop.
+
+				return
+			}
+		}
+	}()
+}
+
+// Stop tells the worker to stop listening for work requests.
+//
+// Note that the worker will only stop *after* it has finished its work.
+func (w *Worker) Stop() {
+	go func() {
+		w.QuitChan <- true
+	}()
+}
+
+func StartDispatcher(nworkers int, limitW int) {
+	// First, initialize the channel we are going to but the workers' work channels into.
+	WorkerQueue = make(chan chan WorkRequest, nworkers)
+	if limitW > 0 {
+		WorkQueue = make(chan WorkRequest, limitW)
+	}
+	// Now, create all of our workers.
+	for i := 0; i < nworkers; i++ {
+		worker := NewWorker(i+1, WorkerQueue)
+		worker.Start()
+	}
+
+	go func() {
+		for {
+			select {
+			case work := <-WorkQueue:
+
+				go func() {
+					worker := <-WorkerQueue
+
+					worker <- work
+				}()
+			}
+		}
+	}()
+}
