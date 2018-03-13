e57613aa83fbee05f108d00f4f8a12621f510eda
miguelramirez93 <darckzios@gmail.com>
2018-03-13 10:25:03 -0500

    se agrega lib para status de api

---
+++ apiStatusLib/apistatus.go
@@ -0,0 +1,13 @@
+package apistatus
+
+import(
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+)
+
+func Init() {
+	beego.Any("/",func(ctx *context.Context){
+    response := map[string]interface{}{"Status": "Ok"} 
+	ctx.Output.JSON(response,true,true)
+})
+}
