eb92bf021981abb87bc89522f1403f45a67f267e
AlaskaRising <nicostevrr@gmail.com>
2019-05-28 12:00:14 -0500

    Se modifica Script DDL para cambiar el orden de las columnas y definir la expresion regular que separa la informacion correctamente, se actualiza el esquema grafico de forma correspondiente. #4

--- Scripts/DDL.sql
+++ Scripts/DDL.sql
@@ -6,28 +6,30 @@ CREATE DATABASE IF NOT EXISTS logswso2
 
 --Script Creacion Tabla de formato de archivos Json almacenados en S3
 
-CREATE EXTERNAL TABLE IF NOT EXISTS logswso2.registro_logs_v2 (
+CREATE EXTERNAL TABLE IF NOT EXISTS logswso2.logs_wso2 (
   `tid` string,
+  `ip` string,
+  `host` string,
   `date` string,
   `level` string,
   `package` string,
-  `details` string,
-  `ip` string,
-  `host` string 
+  `details` string 
 )
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.RegexSerDe'
 WITH SERDEPROPERTIES (
-  'serialization.format' = '|',
-  'field.delim' = '|',
-  'collection.delim' = '@',
-  'mapkey.delim' = '%'
-) LOCATION 's3://logs-wso2-oas/'
+  'serialization.format' = '1',
+  'input.regex' = '([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)'
+) LOCATION 's3://logs-wso2-oas/cleaned_logs/'
 TBLPROPERTIES ('has_encrypted_data'='false');
 
 --Descripcion de campos 
 
 -- ddl-end --
 COMMENT ON COLUMN logswso2.registro_logs_v2.tid IS 'Identificador pendiente de definicion';
+-- ddl-end --
+COMMENT ON COLUMN logswso2.registro_logs_v2.ip IS 'Direccion IP del ordenador en que se hizo la peticion';
+-- ddl-end --
+COMMENT ON COLUMN logswso2.registro_logs_v2.host IS 'Endpoint de la solicitud';
 -- ddl-end --
 COMMENT ON COLUMN logswso2.registro_logs_v2.date IS 'Fecha en que se genero el registro de auditoria';
 -- ddl-end --
@@ -36,8 +38,4 @@ COMMENT ON COLUMN logswso2.registro_logs_v2.level IS 'Identificador del tipo de 
 COMMENT ON COLUMN logswso2.registro_logs_v2.package IS 'Los tipos de valores que agrupa este campo y su utilidad actualmente son desconocidos';
 -- ddl-end --
 COMMENT ON COLUMN logswso2.registro_logs_v2.details IS 'Registro de la accion que se pretende ejecutar ya sea una descripcion del error o el payload generado desde los API''s';
--- ddl-end --
-COMMENT ON COLUMN logswso2.registro_logs_v2.ip IS 'Direccion IP del ordenador en que se hizo la peticion';
--- ddl-end --
-COMMENT ON COLUMN logswso2.registro_logs_v2.host IS 'endpoint de la solicitud';
 -- ddl-end --
