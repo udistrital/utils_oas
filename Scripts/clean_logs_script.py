6027b0bdcba751a9fb256bc9f9c094b15211e2cb
Maria Ortiz <mariaalejandraortiz0401@gmail.com>
2019-05-29 12:13:44 -0500

    se corrige el script de Python en Lambda para que procese solamente el archivo que acaba de entrar y generó el evento; se creó una variable para manipular el nombre del archivo que entra y que este mismo nombre sea el del nuevo archivo en la nueva carpeta; se probó este script al cargar varios archivos al tiempo y se evidencia que ejecuta la función por cada uno de ellos de forma individual, por lo que no se realiza este control en la función ya que no es necesario #6

--- Scripts/clean_logs_script.py
+++ Scripts/clean_logs_script.py
@@ -5,19 +5,19 @@ def lambda_handler(event, context):
     """Read file from s3 on trigger."""
     s3 = boto3.client("s3")
     if event:
-        file_obj = event["Records"][0]
-        bucketname = str(file_obj['s3']['bucket']['name'])
-        filename = str(file_obj['s3']['object']['key'])
-        print(filename)
+        records = event.get('Records', [])
+        bucketname = records[0]['s3']['bucket']['name']
+        filename = records[0]['s3']['object']['key']
         fileObj = s3.get_object(Bucket=bucketname, Key=filename)
         file_content = fileObj["Body"].read().decode('utf-8')
         # remove spaces
         x = ''.join(file_content.split())
         #remove new lines
         a = x.strip().replace('\n', '')
-        y = a.replace('%', '\n')
+        y = a.replace('@$', '\n')
         #create new cleaned file
-        create_cleaned_logs_file(y,bucketname,'Ejemplo.txt')
+        new_name = filename.split("/")[1]
+        create_cleaned_logs_file(y,bucketname,new_name)
     
 def create_cleaned_logs_file(new_logs,source_bucket_name,source_file_name):
 
