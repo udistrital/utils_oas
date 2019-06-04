d40e42c3dfbab596bc667f86211516988c8ec6dd
Maria Alejandra Ortiz <mariaalejandra9404@gmail.com>
2019-06-04 15:56:33 -0500

    Se modifica función de FunctionAfterExc a ListenRequest, agregando try y catch para manejar excepción de no contar con access token (caso que se puede dar cuando la petición no viene de WSO2). De esta manera, se logra crear log cuando se realizan peticiones directamente de WSO2 a los APIs y cuando se hacen a través de un tercero o de mid apis, todo sin que la librería entre en PANIC; esto se probó de manera local en los API mid y crud de Titán #8

--- middleware.go
+++ middleware.go
@@ -9,76 +9,61 @@ import (
 	//amqp "github.com/streadway/amqp"
 )
 
-/*Variables para la conexión y el canal
-var connection *amqp.Connection
-var chl *amqp.Channel
 
-func failOnError(err error, msg string) {
-	if err != nil {
-		beego.Info("%s: %s", msg, err)
-		beego.Info(fmt.Sprintf("%s: %s", msg, err))
-	}
-}
-*/
+func ListenRequest(ctx *context.Context) {
 
-func FunctionBeforeStatic(ctx *context.Context) {
-	beego.Info("beego.BeforeStatic: Before finding the static file")
-}
-func FunctionBeforeRouter(ctx *context.Context) {
-	beego.Info("beego.BeforeRouter: Executing Before finding router")
-}
-func FunctionBeforeExec(ctx *context.Context) {
+	/*---- Declaración de variables ---- */
 
-	beego.Info("beego.BeforeExec: After finding router and before executing the matched Controller")
-}
+		/*---- Información relacionada a la aplicación y la petición ---- */
+		var app_name 	string  	//Nombre del API al que se le hace la petición
+		var host 		string     	//Host del API
+		var end_point 	string		//End point al que se le realiza la petición
+		var method 		string      //Método REST de la petición
+		var date		string 		//Fecha y hora de la operación
 
-func FunctionAfterExec(ctx *context.Context) {
-
-    //Variable que contiene el nombre del API al que se le hace la petición
-    app_name := beego.AppConfig.Strings("appname")
-	//Host del API
-	host := ctx.Request.Host
-	//Variable que contiene el end point al que se le realiza la petición
-	end_point := ctx.Request.URL.String()
-    //Variable que contiene el método REST de la petición
-	method := ctx.Request.Method
-	//Variable que contiene la fecha y hora de la operación
-	date := time.Now().String()
-
-	
-	//Variable que contiene la IP del usuario   <----- pendiente
-	ip_user := "MyIP"
-	//Variable que contiene el access token de la peticion  <--- Cuando viene de WSO2, se puede obtener. Cuando se realiza por postman, por ejemplo, no
-	access_token := ctx.Request.Header["Authorization"][0]
-	//Variable que define el tipo de aplicación, sistema operativo, provedor del software o laversión del software de la petición del agente de usuario
-	user_agent := ctx.Request.Header["User-Agent"][0]
-	//Variable que contiene el usuario    <----- pendiente
-	user := "MyUser"
-
-	//Variable que contiene el response body del servicio
-	data_response := ctx.Input.Data()
+		/*---- Información relacionada con el usuario ---- */
+		var ip_user      string      //IP del usuario   <----- pendiente
+		var access_token string 	 //Access token asignado al usuario que realiza peticion  	
+		var user_agent   string      //Tipo de aplicación, sistema operativo, provedor del software o laversión del software de la petición del agente de usuario
+		var user 		 string 	 //Nombre de usuario en WSO2 que realiza la petición    <----- pendiente
 		
-	fmt.Println("Nombre API: " ,app_name)    
-	fmt.Println("Host de la petición: " ,host)            
-	fmt.Println("Endpoint ",  end_point) 
-	fmt.Println("method " ,method)  
-	fmt.Println("date  " ,date)  
-	fmt.Println("ip_user" ,ip_user)  
-//	fmt.Println("acces_token: " ,access_token)  
-	fmt.Println("user_agent" ,user_agent)  
-	                                            
-	//fmt.Println(data_response["json"])                                                                      //En las peticiones get y post se ve la data, devuelve OK cuando se hace un post o un delete
-	
-	var log = fmt.Sprintf(`%s@&%s@&%s@&%s@&%s@&%s@&%s@&%s@&%s@&%s@$`, app_name, host,end_point,method,date,ip_user,access_token,user_agent,user,data_response["json"])
-	beego.Info(log)
+		/*---- Información relacionada con el cuerpo de la petición ---- */
+		var data_response  string  //Payload del servicio
+
+		/*---- Asignación de variables ----*/
+		app_name = beego.AppConfig.String("appname")
+		host = ctx.Request.Host
+		end_point = ctx.Request.URL.String()
+		method = ctx.Request.Method
+		date = time.Now().String()
+		ip_user = "MyIP"
+		user_agent = ctx.Request.Header["User-Agent"][0]
+		user = "MyUser"
+		//data_response = ctx.Input.Data()	
+		data_response = "ejemplo"
+
+		// *--------- Se implementa try y catch para cuando la petición NO viene de WSO2 y no se tiene access_token
+		
+		// TRY
+		defer func () {
+			if r := recover(); r != nil {
+				//este es el catch
+				access_token = "NO WSO2"
+				var log = fmt.Sprintf(`%s@&%s@&%s@&%s@&%s@&%s@&%s@&%s@&%s@&%s@$`, app_name, host,end_point,method,date,ip_user,access_token,user_agent,user,data_response)
+				beego.Info(log)
+
+			}
+		}()
+		
+		// CATCH
+		access_token = ctx.Request.Header["Authorization"][0]
+		var log = fmt.Sprintf(`%s@&%s@&%s@&%s@&%s@&%s@&%s@&%s@&%s@&%s@$`, app_name, host,end_point,method,date,ip_user,access_token,user_agent,user,data_response)
+		beego.Info(log)
 	
 
 }
 
-func FunctionFinishRouter(ctx *context.Context) {
-	beego.Info("beego.FinishRouter: After finishing router")
-}
 
 func InitMiddleware() {
-	beego.InsertFilter("*", beego.AfterExec, FunctionAfterExec, false)
+	beego.InsertFilter("*", beego.AfterExec, ListenRequest, false)
 }
