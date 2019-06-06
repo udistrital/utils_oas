45e0b5e794cc31a68e123c62fc9ec072cfecb28a
AlaskaRisings <nicostevrr@gmail.com>
2019-06-06 11:40:34 -0500

    Se adiciona Script DDL para la creacion de la tabla de consulta de logs para API's, en base a la estructura de diez campos definida. #10 :ocean:

---
+++ Scripts/DDL_APIS.sql
@@ -0,0 +1,26 @@
+--Script Creacion DB Athena
+
+CREATE DATABASE IF NOT EXISTS logsapis
+  COMMENT 'DB logs apis'
+  WITH DBPROPERTIES ('creator'='Auditoria', 'Dept.'='CORE');
+
+--Script Creacion Tabla formato de archivos almacenados en S3
+
+CREATE EXTERNAL TABLE IF NOT EXISTS logs.logs_apis_test (
+  `app_name` string COMMENT 'Nombre del API al que se le hace la peticion',
+  `host` string COMMENT 'Host del API',
+  `end_point` string COMMENT 'Endpoint al que se le realiza la peticion',
+  `method` string COMMENT 'Metodo REST de la peticion',
+  `date` string COMMENT 'Fecha y hora de la operacion',
+  `ip_user` string COMMENT 'IP del usuario',
+  `access_token` string COMMENT 'Access token asignado al usuario que realiza peticion',
+  `user_agent` string COMMENT 'Tipo de aplicacion, sistema operativo, proveedor del software o la version del software de la peticion del agente de usuario',
+  `user` string COMMENT 'Nombre de usuario en WSO2 que realiza la peticion',
+  `data_response` string COMMENT 'Payload del servicio' 
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.RegexSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = '1',
+  'input.regex' = '([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)'
+) LOCATION 's3://logs-wso2-oas-2/cleaned_logs/'
+TBLPROPERTIES ('has_encrypted_data'='false');
---
+++ Scripts/DDL_WSO2.sql
@@ -0,0 +1,44 @@
+--Script Creacion DB Athena
+
+CREATE DATABASE IF NOT EXISTS logswso2
+  COMMENT 'DB logs wso2'
+  WITH DBPROPERTIES ('creator'='Auditoria', 'Dept.'='CORE');
+
+--Script Creacion Tabla de formato de archivos Json almacenados en S3
+
+CREATE EXTERNAL TABLE IF NOT EXISTS logs.logs_wso2_final (
+  `tid` string,
+  `ip` string,
+  `host` string,
+  `date` string,
+  `level` string,
+  `package` string,
+  `details_opc` string,
+  `details` string 
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.RegexSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = '1',
+  'input.regex' = '([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)@&([^\\r\\n]*)'
+) LOCATION 's3://logs-wso2-oas/cleaned_logs/'
+TBLPROPERTIES ('has_encrypted_data'='false');
+
+--Descripcion de campos 
+
+-- ddl-end --
+COMMENT ON COLUMN logswso2.registro_logs_v2.tid IS 'Identificador pendiente de definicion';
+-- ddl-end --
+COMMENT ON COLUMN logswso2.registro_logs_v2.ip IS 'Direccion IP del ordenador en que se hizo la peticion';
+-- ddl-end --
+COMMENT ON COLUMN logswso2.registro_logs_v2.host IS 'Endpoint de la solicitud';
+-- ddl-end --
+COMMENT ON COLUMN logswso2.registro_logs_v2.date IS 'Fecha en que se genero el registro de auditoria';
+-- ddl-end --
+COMMENT ON COLUMN logswso2.registro_logs_v2.level IS 'Identificador del tipo de registro generado, posibles valores ERROR, INFO';
+-- ddl-end --
+COMMENT ON COLUMN logswso2.registro_logs_v2.package IS 'Los tipos de valores que agrupa este campo y su utilidad actualmente son desconocidos';
+-- ddl-end --
+COMMENT ON COLUMN logswso2.registro_logs_v2.details IS 'Parte de la columna Details que se genera en algunos casos de error, en la mayoria de casos estara vacio este campo';
+-- ddl-end --
+COMMENT ON COLUMN logswso2.registro_logs_v2.details IS 'Registro de la accion que se pretende ejecutar ya sea una descripcion del error o el payload generado desde los API''s';
+-- ddl-end --
