193f7a26558b1b5b2a87238ddd9637709cde1eed
Alex Bustos <alexfbp@gmail.com>
2021-09-15 00:57:17 -0500

    fix: identation/spaces (auto by vscode plugin)

--- apiStatusLib/apistatus.go
+++ apiStatusLib/apistatus.go
@@ -1,13 +1,13 @@
 package apistatus
 
-import(
+import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/context"
 )
 
 func Init() {
-	beego.Any("/",func(ctx *context.Context){
-    response := map[string]interface{}{"Status": "Ok"} 
-	ctx.Output.JSON(response,true,true)
-})
+	beego.Any("/", func(ctx *context.Context) {
+		response := map[string]interface{}{"Status": "Ok"}
+		ctx.Output.JSON(response, true, true)
+	})
 }
