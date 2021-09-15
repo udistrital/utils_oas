e46c5502f6fe17956ab49942f8b92261f7953af4
Alex Bustos <alexfbp@gmail.com>
2021-09-15 02:55:31 -0500

    feat: statusCheck can accept a handler

--- apiStatusLib/apistatus.go
+++ apiStatusLib/apistatus.go
@@ -1,13 +1,50 @@
 package apistatus
 
 import (
+	"fmt"
+
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/context"
 )
 
+func statusResponse(status string) map[string]interface{} {
+	return map[string]interface{}{
+		"Status": status,
+	}
+}
+
+var defaultStatusResponse map[string]interface{} = statusResponse("Ok")
+
+func formatErrorResponse(errorMsg interface{}) map[string]interface{} {
+	return statusResponse(fmt.Sprintf("ERROR: %v", errorMsg))
+}
+
+// InitWithHandler accepts a (handler) function that, once performs the
+// healthcheck, returns "nil" when everything is OK.
+func InitWithHandler(statusCheckHandler func() (statusCheckError interface{})) {
+
+	response := defaultStatusResponse
+
+	// "catch"
+	defer func() {
+		if err := recover(); err != nil {
+			response = formatErrorResponse(err)
+		}
+
+		// "finally"
+		beego.Any("/", func(ctx *context.Context) {
+			ctx.Output.JSON(response, true, true)
+		})
+	}()
+
+	// "try"
+	if statusCheckHandler != nil {
+		if err := statusCheckHandler(); err != nil {
+			response = formatErrorResponse(err)
+		}
+	}
+}
+
 func Init() {
-	beego.Any("/", func(ctx *context.Context) {
-		response := map[string]interface{}{"Status": "Ok"}
-		ctx.Output.JSON(response, true, true)
-	})
+	InitWithHandler(nil)
 }
