3079d9327e260ab03452f74ee3af54e0c81f6c4d
a52290451 <cuchitrin555@hotmail.com>
2023-09-19 15:04:20 -0500

    feat: nuevas funcionalidades XRAY

--- xray/xray.go
+++ xray/xray.go
@@ -15,24 +15,26 @@ import (
 	"github.com/aws/aws-xray-sdk-go/xray"
 )
 
-var globalContext context.Context
+var GlobalContext context.Context
 var SegmentName string
 var StatusCode int
 var Seg *xray.Segment
+var URL string
+var Method string
 var Controller string
 
-func InitXRay(segmentName string) error {
-	os.Setenv("AWS_XRAY_NOOP_ID", "false")
+func InitXRay() error {
+	os.Setenv("AWS_XRAY_NOOP_ID", "true")
+	os.Setenv("AWS_XRAY_DEBUG_MODE", "TRUE")
 	XraySess, err := session.NewSessionWithOptions(session.Options{SharedConfigState: session.SharedConfigEnable})
 	if err != nil {
 		return err
 	}
-
 	xray.Configure(xray.Config{
-		DaemonAddr: "ec2-54-162-219-111.compute-1.amazonaws.com:2000", // Dirección y puerto del demonio de X-Ray local
-		//DaemonAddr: "127.0.0.1:2000",
-		LogLevel:  "info", // Nivel de log deseado
-		LogFormat: "json", // Formato de log deseado (text o json)
+		//DaemonAddr: "ec2-54-162-219-111.compute-1.amazonaws.com:2000", // Dirección y puerto del demonio de X-Ray local
+		DaemonAddr: "127.0.0.1:2000",
+		LogLevel:   "debug", // Nivel de log deseado
+		LogFormat:  "json",  // Formato de log deseado (text o json)
 	})
 
 	// S3 and ECS Clients
@@ -44,17 +46,36 @@ func InitXRay(segmentName string) error {
 	xray.AWS(ecsClient.Client)
 
 	fmt.Println("Listed buckets successfully")
-	SegmentName = segmentName
 	beego.InsertFilter("*", beego.BeforeExec, BeginSegment)
 	beego.InsertFilter("*", beego.AfterExec, EndSegment, false)
 	return nil
 }
 
 func BeginSegment(ctx *context2.Context) {
-	ctx2 := ctx.Request.Context()
-	ctx2, seg := BeginSegmentWithContextTP(ctx2, SegmentName, ctx.Request.Method, ctx.Request.URL.String(), StatusCode, ctx.Request.URL.String(), ctx.Request.Header.Values("X-Amzn-Trace-Id"))
+	fmt.Println("CONTEXTO ENTRANTE: ", ctx.Request.Context())
+	SegmentName = ctx.Input.Context.Request.Host
+	URL = "http://" + SegmentName + ctx.Input.Context.Request.URL.String()
+	Method = ctx.Request.Method
+	ctx3, seg := BeginSegmentWithContextTP(ctx.Request.Context(), StatusCode, ctx.Request.Header.Values("X-Amzn-Trace-Id"))
 	Seg = seg
-	SetContext(ctx2)
+	GlobalContext = ctx3
+}
+
+func BeginSubsegment(subsegment, method, URL string, status int) (context.Context, *xray.Segment) {
+
+	globalContext, subseg := xray.BeginSubsegment(GlobalContext, subsegment)
+
+	subseg.HTTP = &xray.HTTPData{
+		Request: &xray.RequestData{
+			Method: method,
+			URL:    URL,
+		},
+		Response: &xray.ResponseData{
+			Status: status,
+		},
+	}
+	return globalContext, subseg
+
 }
 
 func EndSegment(ctx *context2.Context) {
@@ -69,8 +90,8 @@ func EndSegment(ctx *context2.Context) {
 			if err == nil {
 				Seg.HTTP = &xray.HTTPData{
 					Request: &xray.RequestData{
-						Method: ctx.Request.Method,
-						URL:    ctx.Request.URL.String(),
+						Method: Method,
+						URL:    URL,
 					},
 					Response: &xray.ResponseData{
 						Status: num,
@@ -80,10 +101,14 @@ func EndSegment(ctx *context2.Context) {
 
 		}
 	}
+	fmt.Println("----CONTEXT XRAY 5----:", xray.GetSegment(GlobalContext).Name)
+	fmt.Println("----GLOBAL CONTEXT----:", GlobalContext)
+	fmt.Println("----GLOBAL CONTEXT back----:", context.Background())
+	fmt.Println("SEG NAME PRINCI: ", Seg.Name)
 	Seg.Close(nil)
 }
 
-func UpdateState(Method, URL string, status int, err error) {
+func UpdateState(status int, err error) {
 	StatusCode = status
 	Seg.HTTP = &xray.HTTPData{
 		Request: &xray.RequestData{
@@ -94,6 +119,7 @@ func UpdateState(Method, URL string, status int, err error) {
 			Status: StatusCode,
 		},
 	}
+	fmt.Println("----CONTEXT XRAY 4----:", xray.GetSegment(GlobalContext).Name)
 	if status == 500 || status == 501 || status == 502 || status == 503 {
 		Seg.AddError(fmt.Errorf("%v", err))
 		Seg.Close(nil)
@@ -106,23 +132,8 @@ func EvaluateState(status int) {
 	}
 }
 
-func ErrorController5xx(method, url string, err error) {
+func ErrorController5xx(err error) {
 	StatusCode = 500
-	Seg.HTTP = &xray.HTTPData{
-		Request: &xray.RequestData{
-			Method: method,
-			URL:    url,
-		},
-		Response: &xray.ResponseData{
-			Status: StatusCode,
-		},
-	}
-	Seg.AddError(fmt.Errorf("%v", err))
-	Seg.AddMetadata("Error", err)
-	Seg.Close(nil)
-}
-
-func EndSegmentErr(Method, URL string, err interface{}) {
 	Seg.HTTP = &xray.HTTPData{
 		Request: &xray.RequestData{
 			Method: Method,
@@ -133,43 +144,51 @@ func EndSegmentErr(Method, URL string, err interface{}) {
 		},
 	}
 	Seg.AddMetadata("Error", err)
+	Seg.AddError(fmt.Errorf("%v", err))
+	fmt.Println("----CONTEXT XRAY 3----:", xray.GetSegment(GlobalContext).Name)
+	Seg.Close(nil)
+	fmt.Println("context DONE D: ", Seg.ContextDone)
+	fmt.Println("context DONE seg D2:", Seg.Emitted)
+}
+
+func EndSegmentErr(err interface{}) {
+	Seg.HTTP = &xray.HTTPData{
+		Request: &xray.RequestData{
+			Method: Method,
+			URL:    URL,
+		},
+		Response: &xray.ResponseData{
+			Status: StatusCode,
+		},
+	}
+	Seg.AddMetadata("Error", err)
+	fmt.Println("----CONTEXT XRAY 2----:", xray.GetSegment(GlobalContext).Name)
 	Seg.Close(nil)
 }
 
-func BeginSegmentWithContextTP(ctx context.Context, segmentName string, method string, url string, code int, origin string, traceID []string) (context.Context, *xray.Segment) {
+func BeginSegmentWithContextTP(ctx context.Context, code int, traceID []string) (context.Context, *xray.Segment) {
+
+	ctx, seg := xray.BeginSegment(ctx, SegmentName)
+
+	seg.Origin = URL
+
+	seg.HTTP = &xray.HTTPData{
+		Request: &xray.RequestData{
+			Method: Method,
+			URL:    URL,
+		},
+		Response: &xray.ResponseData{
+			Status: code,
+		},
+	}
+	//fmt.Println("----CONTEXT XRAY 1----:", xray.GetSegment(globalContext).Name)
 	if traceID != nil {
 		traceID := strings.Trim(traceID[0], "[]")
 		id, parent := GetTraceIDAndParentID(traceID)
-		ctx, seg := xray.BeginSegment(ctx, segmentName)
-		seg.Origin = origin
-		seg.HTTP = &xray.HTTPData{
-			Request: &xray.RequestData{
-				Method: method,
-				URL:    url,
-			},
-			Response: &xray.ResponseData{
-				Status: code,
-			},
-		}
 		seg.TraceID = id
 		seg.ParentID = parent
-
-		return ctx, seg
-	} else {
-		ctx, seg := xray.BeginSegment(ctx, segmentName)
-		seg.Origin = origin
-		seg.HTTP = &xray.HTTPData{
-			Request: &xray.RequestData{
-				Method: method,
-				URL:    url,
-			},
-			Response: &xray.ResponseData{
-				Status: code,
-			},
-		}
-		return ctx, seg
 	}
-
+	return ctx, seg
 }
 
 func GetTraceIDAndParentID(traceID string) (trace string, parent string) {
@@ -189,14 +208,6 @@ func GetTraceIDAndParentID(traceID string) (trace string, parent string) {
 	} else {
 		return
 	}
-}
-
-func GetContext() context.Context {
-	return globalContext
-}
-
-func SetContext(ctx context.Context) {
-	globalContext = ctx
 }
 
 func GetStatusCode() int {
