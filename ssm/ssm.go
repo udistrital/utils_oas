feefd7c98bfa757314148472bc934d98fc24a0c4
lud <luasanchezl@udistrital.edu.co>
2025-05-03 02:50:42 -0500

    feat AWS SSM
    - Se agrega módulo para traer parámetros del parameter store de aws.
    - En AWS, la definición de la tarea debe tener definida la varible de entorno AWS_REGION

---
+++ ssm/ssm.go
@@ -0,0 +1,31 @@
+package ssm
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
+)
+
+func GetParameterFromParameterStore(paramName string) (string, error) {
+	// Create a new session
+	sess, err := session.NewSession()
+	if err != nil {
+		return "", fmt.Errorf("unable to create session: %w", err)
+	}
+
+	// Create an SSM client from the session
+	ssmClient := ssm.New(sess)
+
+	// Get the parameter
+	output, err := ssmClient.GetParameter(&ssm.GetParameterInput{
+		Name:           aws.String(paramName),
+		WithDecryption: aws.Bool(true),
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to get parameter: %w", err)
+	}
+
+	return *output.Parameter.Value, nil
+}
