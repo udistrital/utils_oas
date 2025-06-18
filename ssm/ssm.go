f85c019a90b13575320135acc9d3f31d86a1d5d2
Lud <luasanchezl@udistrital.edu.co>
2025-06-18 14:44:32 -0500

    devops: Soporte SSM
    - Se agrega funcionalidad para obtener parámetros de SSM

---
+++ ssm/ssm.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
+)
+
+func GetPasswordFromParameterStore(paramName string) (string, error) {
+	// Create a new session
+	sess, err := session.NewSession()
+	if err != nil {
+		return "", fmt.Errorf("unable to create session: %w", err)
+	}
+
+	// Create an SSM client from the session
+	ssmClient := ssm.New(sess)
+
+	// Get the parameter
+	output, err := ssmClient.GetParameter(&ssm.GetParameterInput{
+		Name:           aws.String(paramName),
+		WithDecryption: aws.Bool(true),
+	})
+	if err != nil {
+		return "", fmt.Errorf("ocurrió un error al consultar el parámetro: %w", err)
+	}
+
+	return *output.Parameter.Value, nil
+}
