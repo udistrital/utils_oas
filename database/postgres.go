bd6b7a922b915af319802a8c5f60ec008ef85d44
Ludwing Sánchez <luasanchezl@udistrital.edu.co>
2025-11-05 16:18:49 -0500

    feat: conexión postgres (#71)

--- database/postgres.go
+++ database/postgres.go
@@ -10,29 +10,32 @@ import (
 )
 
 func BuildPostgresConnectionString() (string, error) {
-
 	baseParameterStore := beego.AppConfig.String("parameterStore")
 	if baseParameterStore == "" {
-		err := errors.New("parameterStore no configurado")
-		logs.Critical(err)
-		return "", err
+		logs.Info("usando credenciales locales para la conexión a la base de datos")
+		conn := formatPostgresConnectionString(beego.AppConfig.String("PGuser"), beego.AppConfig.String("PGpass"))
+		return conn, nil
 	}
 
-	parameterStore := "/" + baseParameterStore + "/" + beego.AppConfig.String("appname") + "/db/"
+	parameterBasePath := "/" + baseParameterStore + "/" + beego.AppConfig.String("appname") + "/db/"
 
-	username, err := ssm.GetParameterFromParameterStore(parameterStore + "username")
+	username, err := ssm.GetParameterFromParameterStore(parameterBasePath + "username")
+	if err != nil {
+		logs.Critical("error consultando username: %v", err)
+		return "", errors.New("error consultando credenciales: " + err.Error())
+	}
+
+	password, err := ssm.GetParameterFromParameterStore(parameterBasePath + "password")
 	if err != nil {
 		logs.Critical("error consultando credenciales: %v", err)
 		return "", errors.New("error consultando credenciales: " + err.Error())
 	}
 
-	password, err := ssm.GetParameterFromParameterStore(parameterStore + "password")
-	if err != nil {
-		logs.Critical("error consultando credenciales: %v", err)
-		return "", errors.New("error consultando credenciales: " + err.Error())
-	}
-
-	conn := "postgres://" + username + ":" + url.QueryEscape(password) + "@" + beego.AppConfig.String("PGhost") + ":" + beego.AppConfig.String("PGport") + "/" + beego.AppConfig.String("PGdb") + "?sslmode=disable&search_path=" + beego.AppConfig.String("PGschema")
+	conn := formatPostgresConnectionString(username, password)
 
 	return conn, nil
+}
+
+func formatPostgresConnectionString(username, password string) string {
+	return "postgres://" + username + ":" + url.QueryEscape(password) + "@" + beego.AppConfig.String("PGhost") + ":" + beego.AppConfig.String("PGport") + "/" + beego.AppConfig.String("PGdb") + "?sslmode=disable&search_path=" + beego.AppConfig.String("PGschema")
 }
