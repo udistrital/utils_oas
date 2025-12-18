4e88d8ef81c664b57424a0d97f81756bce74c80a
lud <luasanchezl@udistrital.edu.co>
2025-12-18 01:06:46 -0500

    devops: cabeceras de seguridad
    - Se elimina Clear-Site-Data ya que no se manejan sesiones de usuario
    - Se eliminan Cross-Origin-Embedder-Policy Cross-Origin-Opener-Policy por ser un api que retorna json
    - Se agrega Content-Security-Policy y X-XSS-Protection
    - Se actualiza Cross-Origin-Resource-Policy para ser consistente con la configuración de CORS
    - Se refuerza Strict-Transport-Security

--- security/security_headers.go
+++ security/security_headers.go
@@ -10,14 +10,13 @@ func SetSecurityHeaders() {
 }
 
 func securityHeaders(ctx *context.Context) {
-	ctx.Output.Header("Clear-Site-Data", "'cache', 'cookies', 'storage', 'executionContexts'")
-	ctx.Output.Header("Cross-Origin-Embedder-Policy", "require-corp")
-	ctx.Output.Header("Cross-Origin-Opener-Policy", "same-origin")
-	ctx.Output.Header("Cross-Origin-Resource-Policy", "same-origin")
+	ctx.Output.Header("Content-Security-Policy", "default-src 'none'; frame-ancestors 'none'")
+	ctx.Output.Header("Cross-Origin-Resource-Policy", "cross-origin")
 	ctx.Output.Header("Permissions-Policy", "geolocation=(), microphone=(), camera=()")
 	ctx.Output.Header("Referrer-Policy", "no-referrer")
 	ctx.Output.Header("Server", "")
-	ctx.Output.Header("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+	ctx.Output.Header("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
+	ctx.Output.Header("X-XSS-Protection", "1; mode=block")
 	ctx.Output.Header("X-Content-Type-Options", "nosniff")
 	ctx.Output.Header("X-Frame-Options", "DENY")
 	ctx.Output.Header("X-Permitted-Cross-Domain-Policies", "none")
