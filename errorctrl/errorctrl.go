601a1689ddf639af5bcc76d9812d28004ee4dfc0
JulianAvella <julianavella01@gmail.com>
2026-09-16 11:01:21 -0500

    feat: Migra la funcionalidad de errorctrl en Beego v1 a Beego v2 Refs udistrital/utils_oas#76

---
+++ errorctrl/errorctrl.go
@@ -0,0 +1,80 @@
+package errorctrl
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/beego/beego/v2/core/logs"
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+// ErrorControlController recovers structured errors raised by Error.
+func ErrorControlController(c beego.Controller, controller string) {
+	if err := recover(); err != nil {
+		logs.Error(err)
+
+		appname, _ := beego.AppConfig.String("appname")
+		message := appname + "/" + controller
+		data := normalizeError(err)
+		statusCode := http.StatusInternalServerError
+
+		if localError, ok := err.(map[string]interface{}); ok {
+			if funcion, ok := localError["funcion"].(string); ok {
+				message += "/" + funcion
+			}
+			if localData, ok := localError["err"]; ok {
+				data = normalizeError(localData)
+			}
+			switch status := localError["status"].(type) {
+			case int:
+				statusCode = status
+			case string:
+				if parsedStatus, err := strconv.Atoi(status); err == nil {
+					statusCode = parsedStatus
+				}
+			}
+		}
+
+		c.Data["message"] = message
+		c.Data["data"] = data
+		c.Ctx.Output.SetStatus(statusCode)
+		c.Data["json"] = map[string]interface{}{
+			"Data":    data,
+			"Message": message,
+			"Status":  strconv.Itoa(statusCode),
+			"Success": false,
+		}
+		if err := c.ServeJSON(); err != nil {
+			logs.Error("error al serializar la respuesta de error: %v", err)
+		}
+	}
+}
+
+// ErrorControlFunction recovers an error and repanics it using the standard structure.
+func ErrorControlFunction(funcion string, status string) {
+	if err := recover(); err != nil {
+		panic(Error(funcion, err, status))
+	}
+}
+
+// Error returns an error using the standard structure.
+func Error(funcion string, err interface{}, status string) (outputError map[string]interface{}) {
+	switch localError := err.(type) {
+	case map[string]interface{}:
+		if fun, ok := localError["funcion"].(string); ok {
+			funcion += "/" + fun
+		}
+		if internalError, ok := localError["err"]; ok {
+			err = internalError
+		}
+	}
+
+	return map[string]interface{}{"funcion": funcion, "err": normalizeError(err), "status": status}
+}
+
+func normalizeError(err interface{}) interface{} {
+	if errorValue, ok := err.(error); ok {
+		return errorValue.Error()
+	}
+	return err
+}
